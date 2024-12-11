05c7795928e4b528e2619e4312e7567b099c26b3
Asadbek67 <asadbeksobirov255@gmail.com>
2024-12-11 17:15:48 +0500

    Update views.py

--- project/lesson/views.py
+++ project/lesson/views.py
@@ -4,13 +4,13 @@ from .models import Course, Lesson
 def home(request):
     courses = Course.objects.all()
     lessons = Lesson.objects.all()
-    return render(request, 'courses/home.html', {'courses': courses, 'lessons': lessons})
+    return render(request, 'home.html', {'courses': courses, 'lessons': lessons})
 
 def lesson_list(request, course_id):
     course = get_object_or_404(Course, pk=course_id)
     lessons = course.lessons.all()
-    return render(request, 'courses/lesson_list.html', {'course': course, 'lessons': lessons})
+    return render(request, 'lesson_list.html', {'course': course, 'lessons': lessons})
 
 def lesson_detail(request, lesson_id):
     lesson = get_object_or_404(Lesson, pk=lesson_id)
-    return render(request, 'courses/lesson_detail.html', {'lesson': lesson})
+    return render(request, 'lesson_detail.html', {'lesson': lesson})
